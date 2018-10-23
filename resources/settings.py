5d3aeb04e16485211d0b08825e2e0ed5bd0ae6e0
Jorge Jorge <jorge.jorge@nimbusoft-tech.com>
2018-10-23 16:28:30 -0500

    update settings

--- resources/settings.py
+++ resources/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 ]
 
 GRAPHENE = {
-    'SCHEMA': 'arancel.schema.schema'
+    'SCHEMA': 'resources.schema.schema'
 }
 
 CACHES = {
@@ -65,7 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'arancel.middleware.userAuthMidleware.UserAuthMidleware'
+    'resources.middleware.userAuthMidleware.UserAuthMidleware'
 ]
 
 ROOT_URLCONF = 'resources.urls'
